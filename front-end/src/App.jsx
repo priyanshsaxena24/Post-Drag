341103010a46c3697da59104505bc4f571e1fd13
Priyansh Saxena <priyanshsaxena@Priyanshs-MacBook-Air.local>
2025-07-26 18:34:39 +0530

    Update front-end

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -183,7 +183,8 @@ export default function App() {
           ? { code, input }
           : { code: finalCode };
 
-        const res = await fetch(`http://localhost:8000/api/execute/${language}/`, {
+        const res = await fetch(`https://post-drag.onrender.com/api/execute/${language}/`, {
+
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(payload),
