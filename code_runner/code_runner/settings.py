02d41f75754c8cc0575f11c389e1602a4d38c1c2
Priyansh Saxena <priyanshsaxena@Priyanshs-MacBook-Air.local>
2025-07-26 16:50:52 +0530

    Commit for render

--- code_runner/code_runner/settings.py
+++ code_runner/code_runner/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for code_runner project.
 
@@ -25,7 +27,8 @@ SECRET_KEY = "django-insecure-l33t2df7t_el924)41xz78gr(7)#p@j%f@+9ibbcb241e!b5+b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Application definition
